116e83e75d9e489f7003dae4c7d29dadc87cf9f1
KELOHL21 <kelsyohlsson@gmail.com>
2023-11-30 23:33:08 +0200

    updates

--- src/main.js
+++ src/main.js
@@ -12,13 +12,48 @@ toggleBtn.onclick = function () {
    : 'fa-solid fa-bars fa-2xl'
 }
 
-// Fecthing Product Data
-// let product = null;
-// fetch('./productData/productDetails.json');
-// then(response => reponse.json())
-// .then(
+// Fetching Products
 
-// )
+let products = null;
+fetch('/src/productData/productDetails.json')
+.then(reponse => reponse.json())
+.then(data => {
+   // Putting my data into the products variable that was orginally null
+   products = data;
+   injectData();
+   // addCartToHTML();
+})
+
+// Displaying data
+function injectData() {
+   let dataList = document.querySelector('.productSection');
+   let firstSixProducts = products.slice(0, 6); // Extract the first six products
+
+   firstSixProducts.forEach(product => {
+      let newProduct = document.createElement('div');
+      newProduct.classList.add('productItem');
+
+      newProduct.innerHTML = `
+         <div>
+
+            <div class='imgContainer'>
+               <img src="${product.img}">
+            </div>
+            
+            <h2>${product.title}</h2>
+            <p>${product.price}</p>
+            <div class="detailBtns">
+               <button class="infoBtn" onclick="moreInfo(${product.id})">More Info</button>
+               <button onclick="addCartBtn(${product.id})">Add to cart</button>
+            </div>
+         </div>
+      `;
+
+      dataList.appendChild(newProduct);
+   });
+}
+
+
 
 // Instagram Carousel
 let imgCopy = document.querySelector('.carousel-slide').cloneNode(true);
